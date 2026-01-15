bbca3ef9c5e5e5cb17091da3cff5e9963f1fbf25
Mohamed Kandil <medokandeal7@gmail.com>
2026-01-15 22:34:49 +0200

    refactor: update base_router prefix to prepare for API versioning

--- src/routes/base.py
+++ src/routes/base.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI , APIRouter,Depends
 from helpers.config import get_settings,Settings
 
 base_router = APIRouter(
-    # prefix='/',
+    #prefix='/api/v1',
     # tags=['Base']
 )
 
