612cdbaec84ca43da9fa1d2903aa61aad7d4ccbe
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2026-01-20 13:31:47 +0200

    fix: remove project_id from upload_data response in data router

--- src/routes/data.py
+++ src/routes/data.py
@@ -64,7 +64,6 @@ async def upload_data(request:Request,project_id:str , file:UploadFile,
        content={
           'response': ResponseSignal.FILE_UPLOAD_SUCCESS.value,
           'file_id': file_id,
-          'project_id':str(project.id)
 
        }
     )
