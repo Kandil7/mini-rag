bd2b57fd19177214af6c4d6eca3296a16f63464a
Mohamed Kandil <105888370+Kandil7@users.noreply.github.com>
2026-01-30 16:05:57 +0200

    implement anwser questions endpoint

--- src/models/enums/ResponseSignal.py
+++ src/models/enums/ResponseSignal.py
@@ -18,7 +18,8 @@ class ResponseSignal(Enum):
     VECTORDB_SEARCH_SUCCESS = "vectordb_search_success"
     LLM_RATE_LIMIT_ERROR = "llm_rate_limit_error"
     LLM_API_ERROR = "llm_api_error"
-    
-    
+    RAG_ANSWER_ERROR = "rag_answer_error"
+    RAG_ANSWER_SUCCESS = "rag_answer_success"
+
 
     
